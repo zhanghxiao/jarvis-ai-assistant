16f10815337375688fa2b75d7d2c38e33f4ab602
zhanghxiao <137479165+zhanghxiao@users.noreply.github.com>
2024-09-08 20:29:35 +0800

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import JarvisUI from './components/JarvisUI';
+
+function App() {
+  return (
+    <div className="App">
+      <JarvisUI />
+    </div>
+  );
+}
+
+export default App;
